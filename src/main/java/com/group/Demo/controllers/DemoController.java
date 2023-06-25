1cf974e8858159a072c6219a9cea0c51e901dbea
Bijay Limbu <bijaylimbu11111@gmail.com>
2023-06-25 12:31:20 +0545

    Crud Operation

--- src/main/java/com/group/Demo/controllers/DemoController.java
+++ src/main/java/com/group/Demo/controllers/DemoController.java
@@ -1,12 +1,67 @@
 package com.group.Demo.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.group.Demo.models.Notes;
+import com.group.Demo.repositories.NotesRepository;
+import org.aspectj.weaver.ast.Not;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class DemoController {
+
+    @Autowired
+    NotesRepository notesRepository;
     @GetMapping(value = "/")
     public String Hello(String name, String role){
         return "Hello World";
+    }
+
+    @PostMapping("/add-notes")
+    public ResponseEntity<Notes> addNotes(@RequestBody Notes notes){
+        Notes _notes=notesRepository.save(new Notes(notes.getTitle(), notes.getDescription()));
+        return new ResponseEntity<>(_notes, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/all-notes")
+    public ResponseEntity<List<Notes>> getAllNotes(){
+        List<Notes> notes=notesRepository.findAll();
+        return new ResponseEntity<>(notes, HttpStatus.OK);
+    }
+
+    @GetMapping("/note/{id}")
+    public ResponseEntity<Notes> getNoteById(Long id){
+        Notes note=notesRepository.findById(id).orElse(null);
+        if(note!=null){
+            return  new ResponseEntity<>(note, HttpStatus.OK);
+        }else{
+            return  new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/note/{id}")
+    public  ResponseEntity<HttpStatus> deleteNote(Long id){
+        notesRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+
+    @PutMapping("/note/{id}")
+    public ResponseEntity<Notes> updateNote(Long id, @RequestBody Notes notes){
+       Optional< Notes> _note=notesRepository.findById(id);
+        if(_note!=null){
+            Notes updatedNote=_note.get();
+            updatedNote.setTitle(notes.getTitle());
+            updatedNote.setDescription(notes.getDescription());
+            notesRepository.save(updatedNote);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
