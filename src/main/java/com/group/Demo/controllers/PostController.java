f64590e0e76dd061c8bb184261bc1d6ef88db5fc
Bijay Limbu <bijaylimbu11111@gmail.com>
2023-06-29 12:55:46 +0545

    GET, GET ALL, UPDATE, DELETE on Spring Boot

--- src/main/java/com/group/Demo/controllers/PostController.java
+++ src/main/java/com/group/Demo/controllers/PostController.java
@@ -6,9 +6,10 @@ import com.group.Demo.repositories.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class PostController {
@@ -24,5 +25,49 @@ public class PostController {
         }
         Post _post=postRepository.save(post);
         return new ResponseEntity<>(_post, HttpStatus.CREATED);
+    }
+
+
+    @GetMapping("/get-all-posts")
+    public ResponseEntity<List<Post>> getAllPosts(){
+        return new ResponseEntity<>(postRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/post-by-id/{id}")
+    public ResponseEntity<Post> getPostById(@PathVariable Long id){
+        return  new ResponseEntity<>(postRepository.findById(id).orElse(null), HttpStatus.OK);
+    }
+
+    @PutMapping("/update-post/{id}")
+    public ResponseEntity<Post> updatePost(@PathVariable Long id, @RequestBody Post updatePost){
+        Optional<Post> existingPostOptional=postRepository.findById(id);
+        if(existingPostOptional.isPresent()){
+            Post existingPost=existingPostOptional.get();
+            existingPost.setText(updatePost.getText());
+
+            for(Comments updateComment: updatePost.getComments()){
+                boolean commentExist=false;
+                for(Comments existingComment: existingPost.getComments()){
+                    if(existingComment.getId().equals(updateComment.getId())){
+                        existingComment.setText(updateComment.getText());
+                        commentExist=true;
+                        break;
+                    }
+                }
+
+            }
+            return new ResponseEntity<>(postRepository.save(existingPost), HttpStatus.OK);
+        }return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/delete-post/{id}")
+    public ResponseEntity<Post> deletePost(@PathVariable Long id){
+        Optional<Post> existingPostOptional=postRepository.findById(id);
+        if(existingPostOptional.isPresent()){
+            postRepository.deleteById(id);
+            return new ResponseEntity<>( HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>( HttpStatus.NOT_FOUND);
+        }
     }
 }
